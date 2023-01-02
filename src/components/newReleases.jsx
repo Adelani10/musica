e5cb8aa513f3ad7628d8196a4e13207fb187559b
Ade Lani <ade@example.com>
2023-01-02 23:53:48 +0100

    test

--- src/components/newReleases.jsx
+++ src/components/newReleases.jsx
@@ -5,11 +5,10 @@ export default function NewReleases ({item}) {
     return (
             <Link to="" key={item.id} className="flex flex-col space-y-1 ">
                 <img src={item.image} alt="" className=" rounded-2xl " />
-                <div>
+                <article>
                     <h1 className="text-gray-400 text-xs tracking-tighter">{item.title}</h1>
                     <h4 className="text-gray-500 text-xs">{item.subtitle}</h4>
-                </div>
-                
+                </article>
             </Link>
             
     )
